76fe7ffcc8c13012173b0d2443fdb84bd5bb6022
Sergio Nibon <snibonjr@gmail.com>
2024-12-24 17:12:09 -0300

    Atualização em 21/12/2024

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 console.log('Olá, Javascript')
+
+var useName = 'Sergio Nibon'
+
+document.getElementById('user-name').innerHTML = useName
+
+/*
+
+
+
+var nome = 'Mestre Yoda'
+var idade = 100
+var jedi = true
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof jedi)
+
+
+
+*/
