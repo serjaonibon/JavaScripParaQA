d189d8b00676039715c6398ee77fb2cebe5e27f2
Sergio Nibon <snibonjr@gmail.com>
2024-12-24 18:29:52 -0300

    Atualização em 21/12/2024

--- js/app.js
+++ js/app.js
@@ -1,22 +1,50 @@
 console.log('Olá, Javascript')
 
-var useName = 'Sergio Nibon'
+//var useName = 'Sergio Nibon'
 
-document.getElementById('user-name').innerHTML = useName
+//document.getElementById('user-name').innerHTML = useName
+
+// Variáveis //
 
 /*
-
-
 
 var nome = 'Mestre Yoda'
 var idade = 100
 var jedi = true
 
-
 console.log(typeof nome)
 console.log(typeof idade)
 console.log(typeof jedi)
 
-
-
 */
+
+
+// Operadores de Comparação  //
+
+/*
+var v1 = 5
+var v2 = '5'
+
+var resultado = v1 != v2
+console.log(resultado)
+*/
+
+//function soma (n1, n2) {
+ //   console.log(n1 + n2)
+//}
+//soma(10, 10)
+
+/*
+function boasVindas() {
+    alert('Sejam bem vindos!')
+}
+
+boasVindas()
+*/
+
+function soma(n1, n2) {
+    return n1 + n2
+}
+
+var resultado = soma(5, 5)
+console.log(resultado)
