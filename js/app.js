56b5eac013f0cdaa9aad1d6a83d56ed645d21a15
Sergio Nibon <snibonjr@gmail.com>
2024-12-27 19:40:07 -0300

    Atualização em 25/12/2024

--- js/app.js
+++ js/app.js
@@ -47,6 +47,48 @@ const App = new Vue({
     data: {
         title: 'Star Wars Lego',
         userName: 'Sergio Nibon.',
-        characters: LIST
+        characters: LIST,
+        searchName: ''
+    },
+    methods: {
+        like(userName) {
+            alert(`O personagem, ${userName}, recebeu um like!`)
+        },
+
+        remove(id) {
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.id !== id
+            })
+
+            
+
+            this.characters = result
+
+        },
+
+
+        search() {
+            if (this.searchName === '') {
+               return  alert('O campo de busca é obrigatório')
+            }
+
+
+            const list = this.characters = LIST
+
+            const result = list.filter(item => {
+                return item.nome === this.searchName
+            })
+
+            if(result.length <=0)  {
+                alert('Nenhum registro encontrado!')
+            } else {
+                this.characters = result
+            }
+
+            
+        }
     }
+    
 })
